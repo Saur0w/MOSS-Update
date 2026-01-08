ff950bef349d896c8080ded5995d351519de2ba8
saurow <sthapliyal085@gmail.com>
2026-01-08 20:04:56 +0530

    "Added another component"

---
+++ src/components/Info/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import gsap from "gsap";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+import styles from './style.module.scss';
+
+export default function Info() {
+    return (
+        <>
+            <section className={styles.Info}>
+
+            </section>
+        </>
+    )
+}
